b64adcd8873ca6b4325a203e88156bbec453f200
Drew Williams <15261678+Github-Archives@users.noreply.github.com>
2024-02-02 17:04:57 -0500

    Add Clerk Third-Party Authentication (#6)
    * install @clerk/nextjs
    
    * import and wrap site with ClerkProvider
    
    * working local Clerk OAuth middleware.js
    
    * add Clerk account management button

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      {/* Clerk Manage Account button */}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
