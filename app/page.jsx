11e7aebb73cf4b7e23d02fd76d3cc7a2d12fce91
Drew Williams <15261678+Github-Archives@users.noreply.github.com>
2024-02-04 18:54:36 -0500

    Add Shadcn. Update Middleware to allow public routes (#7)
    * install shadcn
    
    * add shadcn button
    
    * add card
    
    * add shadcn button and card. update middleware to allow Home and Public routes accessible without login
    
    * add Welcome Page title

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,36 @@
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Home = () => {
   return (
     <div>
+      <p className="text-4xl">Welcome Page</p>
+      {/* ! temp location */}
+      <Card>
+        <CardHeader>
+          <CardTitle>CARD TITLE</CardTitle>
+          <CardDescription>
+            CARD DESCRIPTION: All individual card editing options (including
+            deleting this card) are all accessible via inside this card.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="truncate">
+          <p>CARD CONTENT:</p>https://ui.shadcn.com/docs/components/card#usage
+        </CardContent>
+        <CardFooter>CARD FOOTER</CardFooter>
+      </Card>
+      {/* ! temp */}
       <h1>Home</h1>
       {/* Clerk Manage Account button */}
+      {/* TODO: Move <UserButton/> to <Navbar/> */}
       <UserButton afterSignOutUrl="/" />
     </div>
   );
