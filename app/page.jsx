7ccfd5134a4333d2275b62b0ea7386fb55261986
Github-Archives <drewreynerwilliams@gmail.com>
2024-02-04 17:18:40 -0500

    add card

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,32 @@
 import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Home = () => {
   return (
     <div>
+      {/* ! temp */}
+      <Button>Click me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+      {/* ! temp */}
       <h1>Home</h1>
       {/* Clerk Manage Account button */}
       <UserButton afterSignOutUrl="/" />
