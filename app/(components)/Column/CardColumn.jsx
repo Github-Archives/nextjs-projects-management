d235a5f0b98ab7afdb34cda88be9de631f25e81f
Github-Archives <drewreynerwilliams@gmail.com>
2024-02-12 18:59:10 -0500

    initial logic transfer from Task to Card

---
+++ app/(components)/Column/CardColumn.jsx
@@ -0,0 +1,33 @@
+// ! Done (Card)
+
+// "use client";
+import React from "react";
+
+import "./Column.css";
+import { CardTask } from "../Task/CardTask";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+
+export const CardColumn = ({ cards }) => {
+  return (
+    <div className="column">
+      {/* Need to tell SortableContext which items to keep track of as well as strategy for sorting elements. Note: there's also horizontalListSortingStrategy*/}
+      <SortableContext items={cards} strategy={verticalListSortingStrategy}>
+        {cards.map((card) => (
+          <div key={card.id}>
+            <CardTask
+              id={card.id}
+              title={card.title}
+              description={card.description}
+              content={content}
+              footer={footer}
+              key={card.id}
+            />
+          </div>
+        ))}
+      </SortableContext>
+    </div>
+  );
+};
--- app/Board/page.jsx
+++ app/Board/page.jsx
@@ -29,10 +29,36 @@ import { Column } from "../(components)/Column/Column";
 import { Input } from "../(components)/Input/Input";
 
 const Board = () => {
+  // ? I Want Cards to replace these `tasks`
   const [tasks, setTasks] = useState([
     { id: 1, title: "Add tests to homepage" },
     { id: 2, title: "Fix styling in about section" },
     { id: 3, title: "Learn how to center a div" },
+  ]);
+
+  // ! Card Remake (Task)
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      title: "Title 1",
+      description: "Description 1",
+      content: "Content 1",
+      footer: "Footer 1",
+    },
+    {
+      id: 2,
+      title: "Title 2",
+      description: "Description 2",
+      content: "Content 2",
+      footer: "Footer 2",
+    },
+    {
+      id: 3,
+      title: "Title 3",
+      description: "Description 3",
+      content: "Content 3",
+      footer: "Footer 3",
+    },
   ]);
 
   // Add tasks
@@ -45,8 +71,19 @@ const Board = () => {
     setTasks((tasks) => [...tasks, { id: tasks.length + 1, title }]);
   };
 
+  // ! Card Remake (Add Task)
+  const addCard = (title) => {
+    setCards((cards) => [
+      ...cards,
+      { id: cards.length + 1, title, description, content, footer },
+    ]);
+  };
+
   // Helper function that takes id of task, goes through the 'tasks' array and finds where the id occurs
   const getTaskPosition = (id) => tasks.findIndex((task) => task.id === id);
+
+  // ! Card Remake (getTaskPosition)
+  const getCardPosition = (id) => cards.findIndex((card) => card.id === id);
 
   const handleDragEnd = (event) => {
     console.log("onDragEnd", event);
@@ -70,6 +107,29 @@ const Board = () => {
     }
   };
 
+  // ! Card Remake (handleDragEnd)
+  const cardHandleDragEnd = (event) => {
+    console.log("onDragEnd", event);
+    // active = element currently dragging
+    // over = element which will be replaced, once we let go of the active element
+    const { active, over } = event;
+    // This means it's being let go at the same position it came from. If so do nothing
+    if (active.id === over.id) {
+      return;
+      // Else: setTasks = update the 'tasks' array
+    } else {
+      setCards((cards) => {
+        // Gets position of element before it was dragged
+        const originalPosition = getCardPosition(active.id);
+        // Gets the new position of the element, where it should be after the array is updated
+        const newPosition = getCardPosition(over.id);
+        // Here DND-Kit gives us a utility funciton that updates array based on original & new position
+        // array to update, original pos, new pos
+        return arrayMove(cards, originalPosition, newPosition);
+      });
+    }
+  };
+
   // This is so DND-Kit works on Mobile and Keyboard
   const sensors = useSensors(
     useSensor(PointerSensor),
@@ -79,9 +139,17 @@ const Board = () => {
     }),
   );
 
+  // ! Card Remake (sensors)
+  // * SAME SAME
+
   return (
     <div>
       <p className="text-4xl">Task Board</p>
+      {/* <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      > */}
       <Card>
         <CardHeader>
           <CardTitle>CARD TITLE</CardTitle>
@@ -90,23 +158,24 @@ const Board = () => {
             deleting this card) are all accessible via inside this card.
           </CardDescription>
         </CardHeader>
-        <CardContent>
-          <p>CARD CONTENT:</p>
-          <div className="flex items-center space-x-2">
-            <UserIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-            <span className="text-sm">Assignee: John Doe</span>
-          </div>
-          <div className="flex items-center space-x-2">
-            <CalendarIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-            <span className="text-sm">Due Date: February 20, 2024</span>
-          </div>
-          <div className="flex items-center space-x-2">
-            <CheckIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-            <span className="text-sm">Status: In Progress</span>
-          </div>
-        </CardContent>
-        <CardFooter>CARD FOOTER</CardFooter>
+        {/* <CardContent>
+            <p>CARD CONTENT:</p>
+            <div className="flex items-center space-x-2">
+              <UserIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+              <span className="text-sm">Assignee: John Doe</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <CalendarIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+              <span className="text-sm">Due Date: February 20, 2024</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <CheckIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+              <span className="text-sm">Status: In Progress</span>
+            </div>
+          </CardContent>
+          <CardFooter>CARD FOOTER</CardFooter> */}
       </Card>
+
       {/* Returns the closest rectangles from an array of rectangles to the center of a given..Whenever we drag an element into a certain area collisionDetection decides which area it should go towards when mouse is unclicked*/}
       <DndContext
         sensors={sensors}
