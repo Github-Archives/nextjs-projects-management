60154b9a2367810a980beef11f74dfa389b6dbe4
Github-Archives <drewreynerwilliams@gmail.com>
2024-03-23 14:25:23 -0400

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
 };
